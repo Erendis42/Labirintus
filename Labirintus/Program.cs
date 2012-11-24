f620cda6fcb65d35bbc07ddf257d854a6c313300
erendis42 <erendis42@gmail.com>
2012-11-24 02:17:58 +0100

    :

---
+++ Labirintus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labirintus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Játék játék;
+            try { játék = new Játék(Kommunikáció.Bekér()); }
+            catch { játék = new Játék(25); }
+            játék.Start();
+            Kommunikáció.Vége();
+        }
+    }
+}
