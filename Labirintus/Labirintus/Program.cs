e5ea41b21546044a1ea6d1ee6204412f03587f78
Erendis <mailbox.novaka@gmail.com>
2014-05-07 02:53:09 +0200

    new .exe in .../Release folder i forgot to build before

---
+++ Labirintus/Labirintus/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labirintus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Játék játék;
+            try { játék = new Játék(Kommunikáció.Bekér()); }
+            catch { játék = new Játék(25); }
+            játék.Start();
+            Kommunikáció.Vége();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Labirintus/README.md
@@ -0,0 +1,15 @@
+SSUD
+====
+
+console maze game written in C#
+
+sorry for Hungarian! this is my first homework @ uni, semester 1
+
+you should learn our language anyway because it's nice and hard
+(since it has ancient gibberish roots :P)
+
+you'll find an .exe in "Labirintus/bin/Release" folder. have fun!
+
+TODO:
+
+http://mazeworks.com/mazegen/index.htm
